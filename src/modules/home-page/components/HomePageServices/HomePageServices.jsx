768bf8142c2762637da342555ef884129642d1f9
Nikita Shijan <nikusia0804@gmail.com>
2019-07-24 18:33:13 +1000

    Начал homepageservices

--- src/modules/home-page/components/HomePageServices/HomePageServices.jsx
+++ src/modules/home-page/components/HomePageServices/HomePageServices.jsx
@@ -7,32 +7,32 @@ export const HomePageServices = () => (
     <h2>Услуги</h2>
     <ul className="service-list">
       <li className="service-list__item">
-        <h3>Доставка</h3>
         <button>Доставка</button>
-        <p>
-          Мы с удовольствием доставим ваш товар прямо к вашему подъезду совершенно бесплатно! Ведь
-          мы неплохо заработаем, поднимая его на ваш этаж.
+        <h3 className="h3-1">Доставка</h3>
+        <p className="p1">
+          Мы с удовольствием доставим ваш товар прямо к вашему <br /> подъезду совершенно бесплатно!
+          Ведь мы неплохо <br /> заработаем, поднимая его на ваш этаж.
         </p>
-        <img src="assets/images/delivery.png" alt="Доставка" />
+        <img className="img1" src="assets/images/delivery.png" alt="Доставка" />
       </li>
       <li className="service-list__item">
-        <h3>Гарантия</h3>
         <button>Гарантия</button>
-        <p className="text-about">
-          Если из-за возгорания купленного у нас товара у вас сгорит дом — не переживайте, мы
-          выдадим вам новый. Товар, не дом, конечно же.
+        <h3 className="h3-2">Гарантия</h3>
+        <p className="p2">
+          Если из-за возгорания купленного у нас товара у вас сгорит <br /> дом — не переживайте, мы
+          выдадим вам новый. <br /> Товар, не дом, конечно же.
         </p>
-        <img src="assets/images/warranty.png" alt="Гарантия" />
+        <img className="img2" src="assets/images/warranty.png" alt="Гарантия" />
         <br />
       </li>
       <li className="service-list__item">
-        <h3>Кредит</h3>
         <button>Кредит</button>
-        <p className="text-about">
-          Залезть в долговую яму стало проще! Кредитные консультанты подберут для вас наиболее
-          выгодные условия кредита. Выгодные для нашего банка, разумеется.
+        <h3 className="h3-3">Кредит</h3>
+        <p className="p3">
+          Залезть в долговую яму стало проще! Кредитные <br /> консультанты подберут для вас
+          наиболее выгодные <br /> условия кредита. Выгодные для нашего банка, разумеется.
         </p>
-        <img src="assets/images/credit.png" alt="Кредит" />
+        <img className="img3" src="assets/images/credit.png" alt="Кредит" />
       </li>
     </ul>
   </section>
